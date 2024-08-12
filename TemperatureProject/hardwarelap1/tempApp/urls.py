ba1d6b822ae19fddffef9df51551eb2f1b8847b7
euro <129367034+Euro-mountanio@users.noreply.github.com>
2024-08-12 16:05:42 +0200

    Add files via upload

--- TemperatureProject/hardwarelap1/tempApp/urls.py
+++ TemperatureProject/hardwarelap1/tempApp/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-
-
+from . import views
+from .api import api
 
 
 urlpatterns = [
+    path('api/', api.urls),
+    path('', views.show_temp, name='home'),
     path('admin/', admin.site.urls),
-
 ]
